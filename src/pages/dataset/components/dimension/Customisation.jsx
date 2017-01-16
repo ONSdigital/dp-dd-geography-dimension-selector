4cd9f36560c4b4227dfab094af861a36aadd2817
Marek <mkarwowski@equalexperts.com>
2017-01-16 09:54:28 +0000

    Dimension customisation screen

--- src/pages/dataset/components/dimension/Customisation.jsx
+++ src/pages/dataset/components/dimension/Customisation.jsx
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 import config from '../../../../config';
 
 
-export default class Customisation extends Component {
+class Customisation extends Component {
     constructor(props) {
         super(props);
         this.selectAll = this.selectAll.bind(this);
@@ -26,9 +26,34 @@ export default class Customisation extends Component {
 
                 <h3>Customise location</h3>
 
+                <p>
+                    <Link to={`${basePath}/search`}>Search</Link><br />
+                    Search for specific location
+                </p>
+
+                <p>
+                    <Link to={`${basePath}/browse`}>Browse</Link><br />
+                    Search for location from the list
+                </p>
+
+                <p>
+                    <a onClick={this.selectAll}>All locatinos</a><br />
+                    Add all locations in the dataset
+                </p>
             </div>
         )
     }
 }
 
 
+function mapStateToProps(state, ownProps) {
+    const dimension = state.dataset.dimensions.find((dimension) => {
+        return dimension.id === ownProps.dimensionID;
+    });
+
+    return {
+        dimension
+    }
+}
+
+export default connect(mapStateToProps)(Customisation)
