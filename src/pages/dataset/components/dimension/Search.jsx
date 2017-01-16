ac2e3bfb1803b5a1f70d0618ee72f218815b2ca0
Marek <mkarwowski@equalexperts.com>
2017-01-16 11:59:49 +0000

    Dummy search component

--- src/pages/dataset/components/dimension/Search.jsx
+++ src/pages/dataset/components/dimension/Search.jsx
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { Link } from 'react-router';
+import { Link, browserHistory } from 'react-router';
 import { connect } from 'react-redux';
 
 
@@ -10,6 +10,17 @@ export default class Search extends Component {
 
 
     render () {
-        return <h3>Search</h3>
+        return (
+            <div className="margin-top--2 margin-bottom--4">
+                <Link onClick={browserHistory.goBack} className="btn--everything">Back</Link>
+
+                <h3>What location do you want?</h3>
+                <input type="text" style={{padding: "6px 12px"}}/>
+                <div className="margin-top--2 margin-bottom--4">
+                    <a className="btn btn--primary btn--thick btn--wide btn--big margin-right--half"
+                       onClick={this.saveSelections}>Save selection</a>
+                </div>
+            </div>
+        )
     }
 }
