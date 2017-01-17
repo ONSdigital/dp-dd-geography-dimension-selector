2ed1e0f43fba485bfe1c0d76604fdb95d7b5c409
Marek <mkarwowski@equalexperts.com>
2017-01-17 14:53:20 +0000

    Search call placeholder method

--- src/pages/dataset/components/dimension/Search.jsx
+++ src/pages/dataset/components/dimension/Search.jsx
@@ -1,13 +1,15 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import { Link, browserHistory } from 'react-router';
-import { connect } from 'react-redux';
-
 
 export default class Search extends Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.search = this.search.bind(this);
     }
 
+    search () {
+        throw new Error('Not implemented yet.');
+    }
 
     render () {
         return (
@@ -18,7 +20,7 @@ export default class Search extends Component {
                 <input type="text" style={{padding: "6px 12px"}}/>
                 <div className="margin-top--2 margin-bottom--4">
                     <a className="btn btn--primary btn--thick btn--wide btn--big margin-right--half"
-                       onClick={this.saveSelections}>Save selection</a>
+                       onClick={this.search}>Search</a>
                 </div>
             </div>
         )
